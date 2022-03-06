465b4aff8c45b4446b8522673f6eae71597f816e
Anirudh Rao <anirudh131299@gmail.com>
2022-03-06 20:34:48 +0530

    Added commnets

--- 7.MoreOnChannels/1.Select.go
+++ 7.MoreOnChannels/1.Select.go
@@ -7,19 +7,23 @@ import (
 
 func main() {
 
+	//Making two channels c1
 	c1 := make(chan string)
 	c2 := make(chan string)
 
+	//Go routine for c1
 	go func() {
 		time.Sleep(1 * time.Second)
 		c1 <- "one"
 	}()
+	//Go routine for c2
 	go func() {
 		time.Sleep(2 * time.Second)
 		c2 <- "two"
 	}()
 
 	for i := 0; i < 2; i++ {
+		//Selection of channels
 		select {
 		case msg1 := <-c1:
 			fmt.Println("received", msg1)
