e18ab29b1024ae5b4e920242fcb10e552dd36ac8
Anirudh Rao <anirudh131299@gmail.com>
2022-04-02 14:50:13 +0530

    CrudAPI

--- go-CrudAPI/main.go
+++ go-CrudAPI/main.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"github.com/gorilla/mux"
+	"fmt"
+	"log"
+	"encoding/json"
+	"math/rand"
+	"net/http"
+	"strconv"
+
 )
+type Movie struct{
+	//We need to convert all the fields into JSON
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director  struct{
+	FirstName string `json:"firstname"`
+	LastName string  `json:"lastname"`
+
+}
+
+var movies[] Movie
 
 func main(){
+	r :=mux.NewRouter()
+	r.HandleFunc()
+
 
 }
