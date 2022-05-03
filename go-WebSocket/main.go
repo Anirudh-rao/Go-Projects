0af295ba484ec03a40839cf11bd842105bc2973f
Anirudh Rao <anirudh131299@gmail.com>
2022-05-03 23:18:01 +0530

    WebSocket Project Created

---
+++ go-WebSocket/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+
+	socketio "github.com/googollee/go-socket.io"
+)
+
+func main() {
+	server, err := socketio.NewServer(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	server.On("connection", func(so socketio.Socket) {
+		log.Println("on Connection")
+
+		so.Join("chat")
+
+		so.on("chat message", func(msg string) {
+			log.Println("emit:", so.Emit("chat message", msg))
+			so.BroadcastTo("chat", "chat message", msg)
+		})
+		so.On("disconnection", func() {
+			log.Println("on disconnect")
+		})
+	})
+}
